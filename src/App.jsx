8ae3dfb93fadc5f830a1d9cd46aae783c0067023
Anna Fedorenko <fedorenkoann98@gmail.com>
2024-03-17 15:04:11 +0100

    Add Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
 
+import Profile from "./components/Profile/Profile";
+import userData from "./components/Profile/userData.json";
+import FriendList from "./components/Friendlist/FriendList";
+import friends from "./components/Friendlist/friends.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./components/TransactionHistory/transactions.json";
+
+const App = () => {
   return (
-    <div>
-      Hello world
-    </div>
-  )
-}
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
 
-export default App
+export default App;
